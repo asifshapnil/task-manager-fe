38aed8f5486f20a19efa9a555d249d0f03d3d2cf
asif adham <asif@alagzoo.com>
2024-07-07 23:55:11 +0600

    logo dynamically changing on responsive test.

--- src/core/common/textlogo.component.tsx
+++ src/core/common/textlogo.component.tsx
@@ -1,28 +1,53 @@
+import './textlogo.scss';
+
 const TextLogo = () => {
     return <>
-        <div>
-                            <div className="">
-                                <span style={{
-                                    fontSize: '40px',
-                                    fontFamily: 'monospace',
-                                    fontWeight: '700',
-                                    color: '#fff',
-                                    letterSpacing: '5px'
-                                }}>
-                                    Task
-                                </span>
-                                <span style={{
-                                    fontSize: '20px',
-                                    fontFamily: 'sans-serif',
-                                    fontWeight: '700',
-                                    color: '#fff',
-                                    marginLeft: '.5rem',
-                                    letterSpacing: '5px'
-                                }}>
-                                    Manager
-                                </span>
-                            </div>
-                        </div>
+        <div className="logo-xl">
+            <div className="">
+                <span style={{
+                    fontSize: '40px',
+                    fontFamily: 'monospace',
+                    fontWeight: '700',
+                    color: '#fff',
+                    letterSpacing: '5px'
+                }}>
+                    Task
+                </span>
+                <span style={{
+                    fontSize: '20px',
+                    fontFamily: 'sans-serif',
+                    fontWeight: '700',
+                    color: '#fff',
+                    marginLeft: '.5rem',
+                    letterSpacing: '5px'
+                }}>
+                    Manager
+                </span>
+            </div>
+        </div>
+        <div className="logo-sm">
+            <div className="">
+                <span style={{
+                    fontSize: '40px',
+                    fontFamily: 'monospace',
+                    fontWeight: '700',
+                    color: '#fff',
+                    letterSpacing: '5px'
+                }}>
+                    T
+                </span>
+                <span style={{
+                    fontSize: '20px',
+                    fontFamily: 'sans-serif',
+                    fontWeight: '700',
+                    color: '#fff',
+                    marginLeft: '.5rem',
+                    letterSpacing: '5px'
+                }}>
+                    M
+                </span>
+            </div>
+        </div>
     </>
 }
 
